e6c0dbc6f8737777a218a22673738c274d726db8
panli889 <panli889@gmail.com>
2017-06-05 19:07:30 +0800

    fix npm build error

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /go/src/github.com/laincloud/
 ln -sf /lain/app /go/src/github.com/laincloud/lvault
+cd $(npm root -g)/npm && npm install fs-extra && sed -i -e s/graceful-fs/fs-extra/ -e s/fs\.rename/fs.move/ ./lib/utils/rename.js && cd - # fix npm error
 gobuildweb dist
 unzip -o /lain/app/lvault-0.1.zip -d /lain/app
 cd /lain/app/lvault-0.1
